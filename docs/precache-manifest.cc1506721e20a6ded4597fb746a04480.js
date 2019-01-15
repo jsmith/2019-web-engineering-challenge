be1c31caed01cc37099035c5f6c2e5658b663bf0
Jacob Smith <jacob.smith@unb.ca>
2019-01-15 16:35:20 -0400

    Fixing public path

---
+++ docs/precache-manifest.cc1506721e20a6ded4597fb746a04480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/2019-web-engineering-challenge/robots.txt"
+  },
+  {
+    "revision": "96ecc764621f46e67821",
+    "url": "/2019-web-engineering-challenge/js/chunk-vendors.f562c39b.js"
+  },
+  {
+    "revision": "a10f44b14cad6bbd6469",
+    "url": "/2019-web-engineering-challenge/js/app.9bd8491d.js"
+  },
+  {
+    "revision": "c7b07b809aeef803604abef1c52c44aa",
+    "url": "/2019-web-engineering-challenge/index.html"
+  },
+  {
+    "revision": "96ecc764621f46e67821",
+    "url": "/2019-web-engineering-challenge/css/chunk-vendors.d11f382a.css"
+  },
+  {
+    "revision": "a10f44b14cad6bbd6469",
+    "url": "/2019-web-engineering-challenge/css/app.10f310be.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cd2ce758173ccb100159a39fef9e6524.js"
+  "/2019-web-engineering-challenge/precache-manifest.cc1506721e20a6ded4597fb746a04480.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "web-engineering-challenge"});
